9ff01dbd7374e172ed0fe4c1d4633e6454bd605a
lgrube <lgv9483@saintjoe.edu>
2015-09-14 11:45:24 -0500

    setting up basic structure for javascript file

--- js/snake.js
+++ js/snake.js
@@ -1,3 +1,30 @@
 /*luke grube
 code to make snake do stuff for SPA snake
 */
+
+/*-----------------event handler---------------*/
+/*-----------------end event handler-----------*/
+
+/*-------------creating snake----------------*/
+/*-------------end creating snake------------*/
+
+/*------------arrow key movement--------------------*/
+/*------------end of arrow key movement-------------*/
+
+/*----------------snake movement----------*/
+/*----------------end snake movement------*/
+
+/*-----------------eat block -------------*/
+/*----------------end of eat block----------*/
+
+/*-----------------collision---------------*/
+/*-----------------end of collision--------*/
+
+/*-----------------restart----------------*/
+/*-----------------end restart------------*/
+
+/*-----------------eat block position------*/
+/*--------------end eat block position-----*/
+
+/*---------------board-------------------*/
+/*--------------end board---------------*/
